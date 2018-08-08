137c0e5331971a7ab16809cc05972ef432f7e82c
Roman Khomenko <romankhomenko1995@gmail.com>
2018-08-08 23:08:13 +0300

    Added connection for Student

--- Core/Student.cs
+++ Core/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using Newtonsoft.Json;
 
@@ -9,23 +10,32 @@ namespace MAIAIBot.Core
         public Student(string id,
                        string name,
                        string group,
-                       string[] imgUrls)
+                       string[] imgUrls,
+                       object connection)
         {
             Id = id;
             Name = name;
             Group = group;
-            ImgUrls = imgUrls;
+            ImgUrls.AddRange(imgUrls);
+            Connection = connection;
             Visits = null;
             Variants = null;
         }
 
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
+
         public string Name { get; set; }
+
         public string Group { get; set; }
-        public string[] ImgUrls { get; set; }
-        public DateTime[] Visits { get; set; }
+
+        public List<string> ImgUrls { get; set; }
+
+        public List<DateTime> Visits { get; set; }
+
         public int[] Variants { get; set; }
+
+        public object Connection { get; set; }
 
         public override string ToString()
             => JsonConvert.SerializeObject(this);
