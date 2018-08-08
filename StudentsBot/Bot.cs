275cc33f61c7db1d992b1d1caaec5fffef4cff2b
Roman Khomenko <romankhomenko1995@gmail.com>
2018-08-08 23:09:02 +0300

    Added photo attachments prompt

--- StudentsBot/Bot.cs
+++ StudentsBot/Bot.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.Bot;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Core.Extensions;
@@ -17,6 +18,7 @@ namespace MAIAIBot.StudentsBot
         public const string GatherInfo = "gatherInfo";
         public const string NamePrompt = "namePrompt";
         public const string GroupPrompt = "groupPrompt";
+        public const string PhotoPrompt = "photoPrompt";
     }
 
     public class Bot : IBot
@@ -34,47 +36,69 @@ namespace MAIAIBot.StudentsBot
 
         private async Task NameValidator(ITurnContext context, TextResult result)
         {
-            await NonEmptyStringValidator(context, result, "Введите корректные ФИО!");
+            await NonEmptyStringValidator(context, result, "Введи корректные ФИО!");
         }
 
         private async Task GroupValidator(ITurnContext context, TextResult result)
         {
-            await NonEmptyStringValidator(context, result, "Введите корректный номер группы!");
+            await NonEmptyStringValidator(context, result, "Введи корректный номер группы!");
         }
 
         private async Task AskNameStep(DialogContext dialogContext, object result, SkipStepFunction next)
         {
-            await dialogContext.Prompt(PromptStep.NamePrompt, "Введите ФИО:");
+            await dialogContext.Prompt(PromptStep.NamePrompt, "Напиши ФИО:");
         }
 
         private async Task AskGroupStep(DialogContext dialogContext, object result, SkipStepFunction next)
         {
             var state = dialogContext.Context.GetConversationState<BotState>();
             state.Name = (result as TextResult).Value;
-            await dialogContext.Prompt(PromptStep.NamePrompt, "Введите номер группы:");
+            await dialogContext.Prompt(PromptStep.NamePrompt, "Напиши номер группы:");
+        }
+
+        private async Task AskPhotoStep(DialogContext dialogContext, object result, SkipStepFunction next) {
+            var state = dialogContext.Context.GetConversationState<BotState>();
+            state.Group = (result as TextResult).Value.Trim();
+            await dialogContext.Context.SendActivity("Прикрепи от 2 до 6 фотографий, на которых только ты.");
+        }
+
+        private async Task<IEnumerable<string>> UploadPhotos(DialogContext dialogContext, IEnumerable<Attachment> attachments) {
+            var result = new List<string>();
+
+            foreach (var attachment in attachments) {
+                string url = attachment.ContentUrl;
+                result.Add(url);
+                await dialogContext.Context.SendActivity($"Upload {url}");
+            }
+
+            return result;
         }
 
         private async Task GatherInfoStep(DialogContext dialogContext, object result, SkipStepFunction next)
         {
             var state = dialogContext.Context.GetConversationState<BotState>();
+
+            var attachments = await UploadPhotos(dialogContext, dialogContext.Context.Activity.Attachments);
+
             state.RegistrationComplete = true;
-            state.Group = (result as TextResult).Value.Trim();
             await dialogContext.Context.SendActivity($"Name:\"{state.Name}\", group: {state.Group}");
+            await dialogContext.Context.SendActivity("Отлично! Ты в списке)");
             await dialogContext.End();
         }
 
         public Bot(IDatabaseProvider databaseProvider)
         {
-            dialogs = new DialogSet();
-
             DatabaseProvider = databaseProvider;
+            dialogs = new DialogSet();
 
             dialogs.Add(PromptStep.NamePrompt,
                 new PromptsDialog.TextPrompt(NameValidator));
             dialogs.Add(PromptStep.GroupPrompt,
                 new PromptsDialog.TextPrompt(GroupValidator));
+            dialogs.Add(PromptStep.PhotoPrompt,
+                new PromptsDialog.AttachmentPrompt());
             dialogs.Add(PromptStep.GatherInfo,
-                new WaterfallStep[] { AskNameStep, AskGroupStep, GatherInfoStep });
+                new WaterfallStep[] { AskNameStep, AskGroupStep, AskPhotoStep, GatherInfoStep });
         }
 
         public async Task OnTurn(ITurnContext context)
